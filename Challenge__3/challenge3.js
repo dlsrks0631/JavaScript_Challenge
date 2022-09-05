cff8d10e6b5b56b9e79831735c609a6318c85fd4
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-09-05 21:29:55 +0900

    Update challenge3

--- Challenge__3/challenge3.js
+++ Challenge__3/challenge3.js
@@ -1,16 +1,23 @@
+// value -> string !!
+
 const First_Line_Input = document.querySelector("#First_Line input");
 const Second_Line_Input = document.querySelector("#Second_Line input");
 const Second_Line_Button = document.querySelector("#Second_Line button");
 const Result_First_Line = document.querySelector("#Result_First_Line");
+const Result_Second_Line = document.querySelector("#Result_Second_Line");
 
-function ff() {
+function OnClickEvent() {
   const Max_Num = First_Line_Input.value;
   const Guess_Num = Second_Line_Input.value;
   const Random_Num = Math.floor(Math.random() * Max_Num);
 
-  if (Random_Num !== Guess_Num) {
-    Result_First_Line.innerText = `You chose`;
+  if (parseInt(Guess_Num) === Random_Num) {
+    Result_First_Line.innerText = `You chose ${Guess_Num}, the machine chose: ${Random_Num}`;
+    Result_Second_Line.innerText = `You win!`;
+  } else {
+    Result_First_Line.innerText = `You chose ${Guess_Num}, the machine chose: ${Random_Num}`;
+    Result_Second_Line.innerText = `You lost!`;
   }
 }
 
-Second_Line_Button.addEventListener("click", ff);
+Second_Line_Button.addEventListener("click", OnClickEvent);
